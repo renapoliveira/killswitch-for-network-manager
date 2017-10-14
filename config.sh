0719d51697a94e811e9c39afecf42d810328c5ee
Rena <rena.p.oliveira@gmail.com>
2017-10-14 10:07:26 -0300

    -Config file -Run script -Setting up iptable rules -reconnecting to VPN if it goes down

---
+++ config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#The IP address pf te VPN server
+IP=
+
+#The interface used for the connection (eth0, wlan0, etc)
+INTERFACE=
+
+#The name of the tunnel interface used to route VPN packets
+TUNNEL=
+
+#The name of the already set up VPN
+VPN=
---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+source config.sh
+trap ctrl_c INT
+
+UUID=$(nmcli connection | grep "$VPN" | sed -e 's/'"$VPN"'//g' | awk '{print $1}')
+
+function ctrl_c() {
+	echo "Flushing iptables and exiting"
+	sudo iptables --flush
+	exit 1
+}
+
+sudo iptables --flush
+sudo iptables -A OUTPUT -d $IP -j ACCEPT
+sudo iptables -A INPUT -s $IP -j ACCEPT
+sudo iptables -A OUTPUT -o $TUNNEL -j ACCEPT
+sudo iptables -A INPUT -i $TUNNEL -j ACCEPT
+sudo iptables -A OUTPUT -o $INTERFACE -j DROP
+sudo iptables -A INPUT -i $INTERFACE -j DROP
+
+while [ true ]
+do
+	if [[ $(nmcli connection) != *$TUNNEL* ]]; then
+		nmcli connection up $UUID
+	fi
+	sleep 5
+done
+
